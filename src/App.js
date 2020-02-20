90e81ab7b6592613f8edf0d9ab13ece80e021120
catalin_covic <catalin.covic@gmail.com>
2020-02-19 19:36:47 -0800

    update readme

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React :)
+          auto built github pages by travis :)
         </a>
       </header>
     </div>
