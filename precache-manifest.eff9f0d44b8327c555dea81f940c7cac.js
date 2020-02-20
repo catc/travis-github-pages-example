724e225c96ddd673210c9af2257e709c961b60ec
catalin c <deploy@travis-ci.org>
2020-02-20 03:35:35 +0000

    Deploy catc/travis-github-pages-example to github.com/catc/travis-github-pages-example.git:gh-pages

---
+++ precache-manifest.eff9f0d44b8327c555dea81f940c7cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d261154485ae9b2b4357f684496772",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72a3c2f854c2549230d4",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6e9848ecbc3a47419731",
+    "url": "./static/js/2.aa7eb795.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.aa7eb795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a3c2f854c2549230d4",
+    "url": "./static/js/main.38c7eeab.chunk.js"
+  },
+  {
+    "revision": "319ca390e5a723b49be1",
+    "url": "./static/js/runtime-main.69a38386.js"
+  }
+]);
