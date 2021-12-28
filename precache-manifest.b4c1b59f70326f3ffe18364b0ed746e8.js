09896765172f0f0dc8b63423e62f49bebf6e9da9
catc <catc@users.noreply.github.com>
2021-12-28 21:28:56 +0000

    deploy: 456657fbd3bece2ac15a80223202cc28ee7786cc

---
+++ precache-manifest.b4c1b59f70326f3ffe18364b0ed746e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18cce35c25452521c4a7ebc48a775557",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a967f0ca6471495d314f",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6e9848ecbc3a47419731",
+    "url": "./static/js/2.aa7eb795.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.aa7eb795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a967f0ca6471495d314f",
+    "url": "./static/js/main.985137f9.chunk.js"
+  },
+  {
+    "revision": "319ca390e5a723b49be1",
+    "url": "./static/js/runtime-main.69a38386.js"
+  }
+]);
