10874fb3e7c8724d155df518a052c0cc679ee19c
catalin <catc@users.noreply.github.com>
2021-12-28 13:24:45 -0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/travis/i);
+  const linkElement = getByText(/actions/i);
   expect(linkElement).toBeInTheDocument();
 });
